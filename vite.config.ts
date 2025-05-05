b9a8ed47a5a6cde73b78a941b04a958695cc54ae
ftvhv <154219248+ftvhv@users.noreply.github.com>
2025-05-05 14:12:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,17 @@
 // export default defineConfig({
 //   base: '/lr6/', 
 // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/lr6/',
+//   plugins: [react(), tailwindcss()],
+// });
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  base: '/lr6/',
-  plugins: [react(), tailwindcss()],
+  base: '/lr6/', // Укажите имя вашего репозитория
 });
