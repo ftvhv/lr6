3775eec286b89d81d134b86200078788376bcccb
ftvhv <154219248+ftvhv@users.noreply.github.com>
2025-05-05 14:16:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,16 @@
 //   base: '/lr6/',
 //   plugins: [react(), tailwindcss()],
 // });
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   base: '/lr6/', // Укажите имя вашего репозитория
+// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lr6/', // Укажите имя вашего репозитория
-});
+  base: process.env.NODE_ENV === 'production' ? '/lr6/' : '/',
+  plugins: [react()],
+})
